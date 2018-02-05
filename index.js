517b82e88025d81e9b5dbc04d5affcb697e385d0
Oleksandr Diuriagin <sashadg@gmail.com>
2018-02-05 02:43:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var num = Math.floor(Math.random())
+ var price = Math.floor(Math.random()*100) +1
  var obj = new Object()
  obj[item] = num
 }
