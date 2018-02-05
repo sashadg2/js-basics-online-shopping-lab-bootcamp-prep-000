1acb5f5b915646ae9760fcfa5a17f1e98a266824
Oleksandr Diuriagin <sashadg@gmail.com>
2018-02-05 04:16:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,18 +19,29 @@ function addToCart(item) {
  return cart
 }
 
-function viewCart() {
-  if (cart.length === 0) {
-    console.log("Your shopping cart is empty.")
-  } else {
-    var items = [];
-    for (var i = 0; i < cart.length; i++) {
-      for (var item in cart[i]) {
-        items.push(item + " at $" + cart[i][item])
-      }
-    }
-    console.log("In your cart, you have " + items.join(", ") + ".");
+function viewCart (){
+if(cart.length === 0){
+  console.log(`Your shopping cart is empty.`)
+}
+else{
+      var myString = "In your cart, you have "
+
+        for(var i = 0, l = cart.length; i < l; i++){
+            for(var list in cart[i]){
+            myString+=`${list} at $${cart[i][list]}`
+                if(i!==cart.length-1){
+                  myString+=", "
+                }
+                else{
+                  myString+="."
+                }
+              }
+
+            }
+
+      console.log(myString)
   }
+
 }
 
 function total() {
