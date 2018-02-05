9941d02a9e30235e44c6aa83d7c80e0fe45252c1
Oleksandr Diuriagin <sashadg@gmail.com>
2018-02-05 04:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function viewCart() {
          items.push(item + " at $" + cart[i][item])
        }
      }
-     console.log("In your cart, you have " + items.join(", ") + ".");
+     console.log("In your cart, you have " + items;
    }
 }
 
